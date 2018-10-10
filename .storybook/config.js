a8482c4ff7ee55c6297cfb6668bb4370392ea19e
Kacper Olek <kacper.olek@gmail.com>
2018-10-10 18:51:02 +0200

    jsx->js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../stories/index.jsx');
+  require('../stories/index.js');
   // You can require as many stories as you need.
 }
 
